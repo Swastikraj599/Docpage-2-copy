7be30a3d03ed11db72cf5a46a8e943bb5d01c290
Swastik raj <118122689+Swastikraj599@users.noreply.github.com>
2025-05-27 16:14:50 +0530

    Update Vite configuration

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
+});
